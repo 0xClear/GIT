c8d93fd9238be7b3ee80eca7eab02e45c4799f95
0xClear <bahadoriborz@gmail.com>
2025-04-30 16:12:10 +0200

    First commit

---
+++ GitPractics/src/main/java/de/fsj/service/App.java
@@ -0,0 +1,13 @@
+package de.fsj.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
